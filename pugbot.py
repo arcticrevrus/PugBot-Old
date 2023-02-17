87d541b83032ad610645a7c7b0565a0bae168105
ArcticRevrus <arcticrevrus@gmail.com>
2023-02-17 18:23:29 -0500

    Added message persistent add buttons.

--- pugbot.py
+++ pugbot.py
@@ -2,12 +2,14 @@ import interactions
 import asyncio
 from config import token
 
-bot = interactions.Client(token)
+bot = interactions.Client(token, intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
 
 dps_queue = []
 healer_queue = []
 tank_queue = []
+prevmsg = ""
 lock = asyncio.Lock()
+
 
 dps_button = interactions.Button(
 	style=interactions.ButtonStyle.DANGER,
@@ -27,6 +29,39 @@ healer_button = interactions.Button(
 	custom_id="healer_click",
 )
 
+@bot.event
+async def on_start():
+	for guild in bot.guilds:
+		chans = await guild.get_all_channels()
+		for channel in chans:
+			if channel.name == "mythic-plus-pickup":
+				await channel.send("********Bot has been reloaded********")
+				await channel.send("Click a button to add to the queue.",
+						components=[tank_button, healer_button, dps_button])
+
+
+
+@bot.event
+async def on_message_create(message: interactions.message):
+	global prevmsg
+	chan = await interactions.get(bot, interactions.Channel, object_id=message.channel_id)
+	if str(chan) == "mythic-plus-pickup":
+		if message.author != bot.me:
+			if prevmsg != "":
+				await prevmsg.delete()
+				await chan.send("Click a button to add to the queue.",
+					components=[tank_button, healer_button, dps_button])
+		if message.author == bot.me:
+			if "Click a button to add to the queue." in message.content:
+				prevmsg = message
+			else:
+				if prevmsg != "":
+					await prevmsg.delete()
+					await chan.send("Click a button to add to the queue.",
+						components=[tank_button, healer_button, dps_button])
+			
+
+
 async def queue_check(ctx: interactions.ComponentContext):
 	if(len(tank_queue) >= 1 and len(dps_queue) >= 3 and len(healer_queue) >= 1):
 		for tank in tank_queue:
@@ -45,7 +80,7 @@ async def queue_check(ctx: interactions.ComponentContext):
 									filled_queue_notify.append(f"<@{dps.id}>")
 									filled_queue.append(dps)
 		if(len(filled_queue) == 5):
-			await ctx.send("A group has been found!" + ' , '.join(filled_queue_notify))
+			await ctx.channel.send("A group has been found!" + ' , '.join(filled_queue_notify))
 			for user in filled_queue:
 				if user in tank_queue:
 					tank_queue.remove(user)
@@ -66,11 +101,11 @@ async def _click_me(ctx: interactions.ComponentContext):
 	async with lock: 
 		if user not in dps_queue:
 			dps_queue.append(user)
-			await ctx.send(f"{member} added to queue as dps!")
+			await ctx.channel.send(f"{member} added to queue as dps!")
 			await queue_check(ctx)
 		else:
 			dps_queue.remove(user)
-			await ctx.send(f"{member} has been removed from the dps queue.")
+			await ctx.channel.send(f"{member} has been removed from the dps queue.")
 
 @bot.component("tank_click")
 async def _click_me(ctx: interactions.ComponentContext):
@@ -83,11 +118,11 @@ async def _click_me(ctx: interactions.ComponentContext):
 	async with lock: 
 		if user not in tank_queue:
 			tank_queue.append(user)
-			await ctx.send(f"{member} added to queue as tank!")
+			await ctx.channel.send(f"{member} added to queue as tank!")
 			await queue_check(ctx)
 		else:
 			tank_queue.remove(user)
-			await ctx.send(f"{member} has been removed from the tank queue.")
+			await ctx.channel.send(f"{member} has been removed from the tank queue.")
 
 @bot.component("healer_click")
 async def _click_me(ctx: interactions.ComponentContext):
@@ -100,11 +135,11 @@ async def _click_me(ctx: interactions.ComponentContext):
 	async with lock: 
 		if user not in healer_queue:
 			healer_queue.append(user)
-			await ctx.send(f"{member} added to queue as healer!")
+			await ctx.channel.send(f"{member} added to queue as healer!")
 			await queue_check(ctx)
 		else:
 			healer_queue.remove(user)
-			await ctx.send(f"{member} has been removed from the healer queue.")
+			await ctx.channel.send(f"{member} has been removed from the healer queue.")
 
 
 
